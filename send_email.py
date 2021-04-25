cf5b69475fece288728211071b66aae10bf217fb
LaxmanMaharjan <57948991+LaxmanMaharjan@users.noreply.github.com>
2021-04-25 13:53:29 +0545

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -2,15 +2,17 @@ import smtplib
 
 from email.message import EmailMessage
 
+EMAIL_ADDRESS = os.environ.get('EMAIL_ADDRESS')
+EMAIL_PASSWORD = os.environ.get('EMAIL_PASSWORD')
 
 msg = EmailMessage()
-msg['Subject'] = 'Checking smtplib'
-msg['From'] = 'kce074bct020@gmail.com'
+msg['Subject'] = 'Checking smtplib FROM GITHUB'
+msg['From'] = EMAIL_ADDRESS
 msg['To'] = 'lxmnmrzn@gmail.com'
 
 msg.set_content('This is a email check.')
 
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login("kce074bct020@gmail.com", "khwopacollege9849")
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
     smtp.send_message(msg)
