1c6cc1186f1a40e30a5d34bda67f1644ed53845b
LaxmanMaharjan <57948991+LaxmanMaharjan@users.noreply.github.com>
2021-04-25 13:42:59 +0545

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,23 +1,16 @@
 import smtplib
-import pickle
 
 from email.message import EmailMessage
-
-with open("Gmail.txt","rb") as file:
-    gmail = pickle.load(file)
-    
-EMAIL_ADDRESS = gmail['username']
-EMAIL_PASSWORD = gmail['password']
 
 
 msg = EmailMessage()
 msg['Subject'] = 'Checking smtplib'
-msg['From'] = EMAIL_ADDRESS
+msg['From'] = 'kce074bct020@gmail.com'
 msg['To'] = 'lxmnmrzn@gmail.com'
 
-msg.set_content('This is a email check.' )
+msg.set_content('This is a email check.')
 
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
-    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    smtp.login("kce074bct020@gmail.com", "khwopacollege9849")
     smtp.send_message(msg)
