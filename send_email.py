a1a846cce089e0f1c3cd175498c53a5771026c8f
LaxmanMaharjan <57948991+LaxmanMaharjan@users.noreply.github.com>
2021-04-28 14:25:28 +0545

    Update send_email.py

--- send_email.py
+++ send_email.py
@@ -1,6 +1,12 @@
 import smtplib
 import os
+import pandas as pd
+
 from email.message import EmailMessage
+
+data = pd.read_csv("emails.csv")
+
+email_list = data.Email.tolist()
 
 EMAIL_ADDRESS = os.environ['EMAIL_ADDRESS']
 EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
@@ -8,9 +14,9 @@ EMAIL_PASSWORD = os.environ['EMAIL_PASSWORD']
 msg = EmailMessage()
 msg['Subject'] = 'Checking smtplib FROM GITHUB'
 msg['From'] = EMAIL_ADDRESS
-msg['To'] = 'lxmnmrzn@gmail.com'
+msg['To'] = ", ".join(email_list)
 
-msg.set_content('This is a email check.')
+msg.set_content('This is a email checking from github.')
 
 
 with smtplib.SMTP_SSL('smtp.gmail.com', 465) as smtp:
